58025bb6395bb1fee468f506b08327c0e103eb24
george_spiridakis <spiridakis.grg@gmail.com>
2016-04-23 12:10:28 +0300

    Initial commit.

---
+++ app/src/main/java/com/example/android/fintech_hackathon_wv/WebviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.fintech_hackathon_wv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+    }
+}

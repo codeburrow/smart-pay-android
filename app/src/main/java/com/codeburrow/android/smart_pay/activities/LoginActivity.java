22b2161bc907f53ae4d48c62bbb5bae234f5a317
george_spiridakis <spiridakis.grg@gmail.com>
2016-04-29 19:37:47 +0300

    Implement AsyncResponse so the external use of AttemptToFindAccountTask isavailable.Resolve #17.

--- app/src/main/java/com/codeburrow/android/smart_pay/activities/LoginActivity.java
+++ app/src/main/java/com/codeburrow/android/smart_pay/activities/LoginActivity.java
@@ -3,23 +3,16 @@ package com.codeburrow.android.smart_pay.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.codeburrow.android.smart_pay.R;
-import com.codeburrow.android.smart_pay.api.AccountApi;
-import com.codeburrow.android.smart_pay.api.Api;
+import com.codeburrow.android.smart_pay.async_tasks.AttemptToFindAccountTask;
+import com.codeburrow.android.smart_pay.async_tasks.AttemptToFindAccountTask.AsyncResponse;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements AsyncResponse {
     public static final String PREFERENCES = "Credentials";
     public static final String IBAN_PREFS_KEY = "ibanKey";
     public static final String PASSWORD_PREFS_KEY = "passKey";
@@ -45,58 +38,63 @@ public class LoginActivity extends AppCompatActivity {
         editor.putString(PASSWORD_PREFS_KEY, password);
         editor.commit();
 
-        AttemptToFindAccountTask attemptToFindAccountTask = new AttemptToFindAccountTask(getApplicationContext(), iban);
+        AttemptToFindAccountTask attemptToFindAccountTask = new AttemptToFindAccountTask(getApplicationContext(), this, iban);
         attemptToFindAccountTask.execute();
     }
 
-    public class AttemptToFindAccountTask extends AsyncTask<Void, Void, JSONObject> {
-        private final String TAG = AttemptToFindAccountTask.class.getSimpleName();
-        private final Context mContext;
-        private String iban;
-
-        public AttemptToFindAccountTask(Context context, String iban) {
-            super();
-
-            this.mContext = context;
-            this.iban = iban;
-        }
-
-        @Override
-        protected JSONObject doInBackground(Void... args) {
-            AccountApi accountApi = new AccountApi();
-            return accountApi.findByIban(iban);
-        }
-
-        @Override
-        protected void onPostExecute(JSONObject apiResponse) {
-            if (null == apiResponse) {
-                Toast.makeText(mContext, "No API Response.", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            try {
-                if (!apiResponse.has(Api.ACCOUNTS_KEY)) {
-                    Toast.makeText(mContext, "API Error.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                JSONArray accounts = apiResponse.getJSONArray(Api.ACCOUNTS_KEY);
-
-                if (accounts.length() == 0) {
-                    Toast.makeText(mContext, "IBAN Validation Failed.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                JSONObject account = (JSONObject) accounts.get(0);
-
-                Toast.makeText(mContext, "IBAN validation successful.", Toast.LENGTH_SHORT).show();
-                Toast.makeText(mContext, account.toString(), Toast.LENGTH_LONG).show();
-
-                startActivity(new Intent(mContext, ScanQrCodeActivity.class));
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Log.e(TAG, e.getMessage());
-            }
-        }
+    @Override
+    public void processFinish() {
+        startActivity(new Intent(this, ScanQrCodeActivity.class));
     }
+
+//    public class AttemptToFindAccountTask extends AsyncTask<Void, Void, JSONObject> {
+//        private final String TAG = AttemptToFindAccountTask.class.getSimpleName();
+//        private final Context mContext;
+//        private String iban;
+//
+//        public AttemptToFindAccountTask(Context context, String iban) {
+//            super();
+//
+//            this.mContext = context;
+//            this.iban = iban;
+//        }
+//
+//        @Override
+//        protected JSONObject doInBackground(Void... args) {
+//            AccountApi accountApi = new AccountApi();
+//            return accountApi.findByIban(iban);
+//        }
+//
+//        @Override
+//        protected void onPostExecute(JSONObject apiResponse) {
+//            if (null == apiResponse) {
+//                Toast.makeText(mContext, "No API Response.", Toast.LENGTH_LONG).show();
+//                return;
+//            }
+//
+//            try {
+//                if (!apiResponse.has(Api.ACCOUNTS_KEY)) {
+//                    Toast.makeText(mContext, "API Error.", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//
+//                JSONArray accounts = apiResponse.getJSONArray(Api.ACCOUNTS_KEY);
+//
+//                if (accounts.length() == 0) {
+//                    Toast.makeText(mContext, "IBAN Validation Failed.", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//
+//                JSONObject account = (JSONObject) accounts.get(0);
+//
+//                Toast.makeText(mContext, "IBAN validation successful.", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, account.toString(), Toast.LENGTH_LONG).show();
+//
+//                startActivity(new Intent(mContext, ScanQrCodeActivity.class));
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//                Log.e(TAG, e.getMessage());
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/codeburrow/android/smart_pay/async_tasks/AttemptToFindAccountTask.java
+++ app/src/main/java/com/codeburrow/android/smart_pay/async_tasks/AttemptToFindAccountTask.java
@@ -1,12 +1,10 @@
 package com.codeburrow.android.smart_pay.async_tasks;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.codeburrow.android.smart_pay.activities.ScanQrCodeActivity;
 import com.codeburrow.android.smart_pay.api.AccountApi;
 import com.codeburrow.android.smart_pay.api.Api;
 
@@ -14,53 +12,60 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-//public class AttemptToFindAccountTask extends AsyncTask<Void, Void, JSONObject> {
-//    private final String TAG = AttemptToFindAccountTask.class.getSimpleName();
-//    private final Context mContext;
-//    private String iban;
-//
-//    public AttemptToFindAccountTask(Context context, String iban) {
-//        super();
-//
-//        this.mContext = context;
-//        this.iban = iban;
-//    }
-//
-//    @Override
-//    protected JSONObject doInBackground(Void... args) {
-//        AccountApi accountApi = new AccountApi();
-//        return accountApi.findByIban(iban);
-//    }
-//
-//    @Override
-//    protected void onPostExecute(JSONObject apiResponse) {
-//        if (null == apiResponse) {
-//            Toast.makeText(mContext, "No API Response.", Toast.LENGTH_LONG).show();
-//            return;
-//        }
-//
-//        try {
-//            if (!apiResponse.has(Api.ACCOUNTS_KEY)) {
-//                Toast.makeText(mContext, "API Error.", Toast.LENGTH_LONG).show();
-//                return;
-//            }
-//
-//            JSONArray accounts = apiResponse.getJSONArray(Api.ACCOUNTS_KEY);
-//
-//            if (accounts.length() == 0) {
-//                Toast.makeText(mContext, "IBAN Validation Failed.", Toast.LENGTH_LONG).show();
-//                return;
-//            }
-//
-//            JSONObject account = (JSONObject) accounts.get(0);
-//
-//            Toast.makeText(mContext, "IBAN validation successful.", Toast.LENGTH_SHORT).show();
-//            Toast.makeText(mContext, account.toString(), Toast.LENGTH_LONG).show();
-//
-////            startActivity(new Intent(mContext, ScanQrCodeActivity.class));
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//            Log.e(TAG, e.getMessage());
-//        }
-//    }
-//}
+public class AttemptToFindAccountTask extends AsyncTask<Void, Void, JSONObject> {
+    private final String TAG = AttemptToFindAccountTask.class.getSimpleName();
+    private final Context mContext;
+    private String iban;
+    public AsyncResponse delegate = null;
+
+    // We may separate this or combined to caller class.
+    public interface AsyncResponse {
+        void processFinish();
+    }
+
+    public AttemptToFindAccountTask(Context context, AsyncResponse delegate, String iban) {
+        super();
+
+        this.delegate = delegate;
+        this.mContext = context;
+        this.iban = iban;
+    }
+
+    @Override
+    protected JSONObject doInBackground(Void... args) {
+        AccountApi accountApi = new AccountApi();
+        return accountApi.findByIban(iban);
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject apiResponse) {
+        if (null == apiResponse) {
+            Toast.makeText(mContext, "No API Response.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            if (!apiResponse.has(Api.ACCOUNTS_KEY)) {
+                Toast.makeText(mContext, "API Error.", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            JSONArray accounts = apiResponse.getJSONArray(Api.ACCOUNTS_KEY);
+
+            if (accounts.length() == 0) {
+                Toast.makeText(mContext, "IBAN Validation Failed.", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            JSONObject account = (JSONObject) accounts.get(0);
+
+            Toast.makeText(mContext, "IBAN validation successful.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(mContext, account.toString(), Toast.LENGTH_LONG).show();
+
+            delegate.processFinish();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage());
+        }
+    }
+}
